693b22c55cf6e3c4d55003100285c1d718b7b401
Matthew Yoon <myoon11@my.bcit.ca>
2023-11-18 17:14:26 -0800

    create function make-board

---
+++ make_enemy.py
@@ -0,0 +1,42 @@
+"""
+Tommy Ju
+A01347715
+Matthew Yoon
+A01373290
+"""
+import random
+
+
+def make_enemy(game_board):
+    """
+    create a list of enemy locations
+    :param game_board: dictionary made from make_board function representing play area
+    :precondition: board is dictionary with keys being tuples representing location on board
+    :postcondition: creates the correct amount of enemy randomly assigning a coordinate tuple
+                    there are no duplicate coordinates
+    :return: list of tuples representing enemy coordinates
+
+
+    """
+
+    list_of_coordinates = [coordinate for coordinate in game_board.keys()]
+    # remove starting coordinate
+    list_of_coordinates.remove((0, 0))
+    enemy_count = 0
+    minimum_enemy_amount = (len(game_board.keys()) // 2)
+    enemy_coordinates = []
+    while enemy_count < minimum_enemy_amount:
+        random_coordinate = random.choice(list_of_coordinates)
+        enemy_coordinates.append(random_coordinate)
+        list_of_coordinates.remove(random_coordinate)
+        enemy_count += 1
+
+    return enemy_coordinates
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
