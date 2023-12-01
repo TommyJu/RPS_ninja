4c7eb71e18ffa2b9943b2030b687e3318120d387
Tommy Ju <tommyju25@gmail.com>
2023-11-30 16:59:27 -0800

    Update widgets only on input

--- game.py
+++ game.py
@@ -32,24 +32,72 @@ def game():
     achieved_goal = True
     # GUI
 
+    GUI_HEIGHT = 600
+    GUI_WIDTH = 1000
+    CELL_SIZE = 50
 
-    data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
+    # Configure the root window
     root = tk.Tk()
-    user_interface(root, rows, columns, data)
-    game_thread = threading.Thread(target=game_instance, args=(root, character, achieved_goal, board, end_point, rows, columns)).start()
-    root.after(100, game_thread)
+    root.title("RPS Ninja")
+    root.geometry(f"{GUI_WIDTH}x{GUI_HEIGHT}")
+    root.configure(background='black')
+    # Root window comprised of 2 vertical columns for map and information
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=2)
+
+    canvas = tk.Canvas(root)
+    canvas.grid(row=0, column=0, sticky="nsew")
+
+    # Configure a grid and add data to cells
+    for row in range(rows):
+        # Configure even rows
+        canvas.grid_rowconfigure(row, minsize=CELL_SIZE)
+        for column in range(columns):
+            # Configure even columns
+            canvas.grid_columnconfigure(column, minsize=CELL_SIZE)
+            # Add the frame container widget to each cell to create border effect
+            frame = tk.Frame(canvas, background="green", borderwidth=3, relief="raised")
+            frame.grid(row=row, column=column, sticky="nsew", padx=2, pady=2)
+
+    # Images
+    global level_one_image
+    global ninja_image
+    ninja_image_file = Image.open("./assets/ninja.png")
+    ninja_image = ImageTk.PhotoImage(ninja_image_file.resize((30, 30)))
+    level_one_image_file = Image.open("./assets/forest_level_background.jpeg")
+    level_one_image = ImageTk.PhotoImage(level_one_image_file.resize((2000, 2000)))
+
+
+    # Character widgets
+    ninja = tk.Label(canvas, image=ninja_image, background="white")
+    all_character_widgets = [ninja]
+    data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
+    all_character_widgets[0].grid(row=data["character"][1], column=data["character"][0])
+
+    # Add background image to canvas
+    canvas.create_image(0, 0, image=level_one_image)
+
+
+
+    # data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
+    # user_interface(root, rows, columns, data)
+    threading.Thread(target=game_instance, args=(root, character, achieved_goal, board, end_point, rows, columns, all_character_widgets)).start()
     root.mainloop()
 
 
-def game_instance(root, character, achieved_goal, board, end_point, rows, columns):
+def game_instance(root, character, achieved_goal, board, end_point, rows, columns, all_character_widgets):
     while achieved_goal:
         direction = get_user_choice()
         move_character(character, direction)
         describe_current_location(board, character)
         data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
-        user_interface(root, rows, columns, data)
-        root.update()
+        update_widgets(all_character_widgets, data)
+        # user_interface(root, rows, columns, data)
 
+
+def update_widgets(all_character_widgets, user_interface_data):
+    all_character_widgets[0].grid(row=user_interface_data["character"][1], column=user_interface_data["character"][0])
 def main():
     game()
 
---
+++ update_canvas.py
@@ -0,0 +1,13 @@
+"""
+Tommy Ju
+A01347715
+"""
+
+
+def main():
+    pass
+
+    
+if __name__ == "__main__":
+    main()
+    
