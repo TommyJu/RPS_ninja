481b8d10d6afedb7f6c1e9cd685965943a14ef1b
Matthew Yoon <myoon11@my.bcit.ca>
2023-11-30 20:03:30 -0800

    CREATE enemies_move function

---
+++ enemies_move.py
@@ -0,0 +1,29 @@
+"""
+Tommy Ju
+A01347715
+Matthew Yoon
+A01373290
+"""
+import random
+from validate_move_enemy import validate_move_enemy
+
+
+def enemies_move(enemies, visions, board):
+    for enemy in enemies:
+        index = enemies.index(enemy)
+        possible_directions = validate_move_enemy(enemy, board, enemies)
+        if possible_directions:
+            new_coordinate = random.choice(possible_directions)
+            x_change = new_coordinate[0] - enemy[0]
+            y_change = new_coordinate[1] - enemy[1]
+            old_vision = visions[index]
+            new_vision = [old_vision[0]+x_change, old_vision[1]+y_change]
+            visions[index] = new_vision
+            enemies[index] = new_coordinate
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_enemies_move.py
@@ -0,0 +1,25 @@
+"""
+Tommy Ju
+A01347715
+Matthew Yoon
+A01373290
+"""
+from unittest import TestCase
+from enemies_move import enemies_move
+import random
+from unittest.mock import patch
+
+
+class Test(TestCase):
+
+    @patch('random.choice', side_effect=[[1, 1], [1, 2]])
+    def test_enemies_move_both_move_south(self, _):
+        enemies = [[0, 1], [0, 2]]
+        visions = [[1, 1], [1, 2]]
+        board = {(0, 0): 'string', (0, 1): 'string', (0, 2): 'string',
+                 (1, 0): 'string', (1, 1): 'string', (1, 2): 'string'}
+        expected_enemies = [[1, 1], [1, 2]]
+        expected_visions = [[2, 1], [2, 2]]
+        actual = enemies_move(enemies, visions, board)
+        self.assertEqual(expected_enemies, enemies)
+        self.assertEqual(expected_visions, visions)
