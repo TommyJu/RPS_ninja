16d0fe06b276ff048c16d9f63c72091e8da43f5c
Matthew Yoon <myoon11@my.bcit.ca>
2023-11-30 20:24:04 -0800

    Fix validate_move bug

--- validate_move_enemy.py
+++ validate_move_enemy.py
@@ -14,13 +14,12 @@ def validate_move_enemy(enemy, board, enemies):
     :param enemies:
     :return:
     """
-    possibe_directions = [[enemy[0] - 1, enemy[1]], [enemy[0], enemy[1] + 1], [enemy[0] + 1, enemy[1]],
+    possible_directions = [[enemy[0] - 1, enemy[1]], [enemy[0], enemy[1] + 1], [enemy[0] + 1, enemy[1]],
                           [enemy[0], enemy[1] - 1]]
     direction_choices = []
-    for direction in possibe_directions:
-        direction_choices = []
-        direction_as_set = (direction[0], direction[1])
-        if direction_as_set in board and [direction[0], direction[1]] not in enemies:
+    for direction in possible_directions:
+        direction_as_tuple = (direction[0], direction[1])
+        if direction_as_tuple in board and [direction[0], direction[1]] not in enemies:
             direction_choices.append(direction)
     return direction_choices
 
