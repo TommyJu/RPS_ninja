89a3dd8f4f1b9a26191b46bd9eef623d375f2bc3
Matthew <myoon11@my.bcit.ca>
2023-12-07 01:35:44 -0800

    ADD more unit test to does_player_win function

--- combat/test_does_player_win.py
+++ combat/test_does_player_win.py
@@ -9,22 +9,80 @@ from does_player_win import does_player_win
 
 
 class Test(TestCase):
-    def test_does_player_win_draw_case_True(self):
+
+    def test_does_player_win_rock_draw_case_True(self):
+        expected = True
+        actual = does_player_win(('rock', 9), ('rock', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_rock_draw_case_True_boundary(self):
         expected = True
         actual = does_player_win(('rock', 8), ('rock', 8))
         self.assertEqual(expected, actual)
 
-    def test_does_player_win_draw_case_False(self):
+    def test_does_player_win_rock_draw_case_False(self):
         expected = False
         actual = does_player_win(('rock', 7), ('rock', 8))
         self.assertEqual(expected, actual)
 
-    def test_does_player_win_win_case(self):
+    def test_does_player_win_rock_win_case(self):
         expected = True
         actual = does_player_win(('rock', 7), ('scissor', 8))
         self.assertEqual(expected, actual)
 
-    def test_does_player_win_lose_case(self):
+    def test_does_player_win_rock_lose_case(self):
+        expected = False
+        actual = does_player_win(('rock', 7), ('paper', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_scissor_lose_case(self):
         expected = False
         actual = does_player_win(('scissor', 7), ('rock', 8))
         self.assertEqual(expected, actual)
+
+    def test_does_player_win_scissor_draw_case_True(self):
+        expected = True
+        actual = does_player_win(('scissor', 9), ('scissor', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_scissor_draw_case_True_boundary(self):
+        expected = True
+        actual = does_player_win(('scissor', 8), ('scissor', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_scissor_draw_case_False(self):
+        expected = False
+        actual = does_player_win(('scissor', 7), ('scissor', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_scissor_win_case(self):
+        expected = True
+        actual = does_player_win(('scissor', 7), ('paper', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_paper_lose_case(self):
+        expected = False
+        actual = does_player_win(('paper', 7), ('scissor', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_paper_draw_case_True(self):
+        expected = True
+        actual = does_player_win(('paper', 9), ('paper', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_paper_draw_case_True_boundary(self):
+        expected = True
+        actual = does_player_win(('paper', 8), ('paper', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_paper_draw_case_False(self):
+        expected = False
+        actual = does_player_win(('paper', 7), ('paper', 8))
+        self.assertEqual(expected, actual)
+
+    def test_does_player_win_paper_win_case(self):
+        expected = True
+        actual = does_player_win(('paper', 7), ('rock', 8))
+        self.assertEqual(expected, actual)
+
+
