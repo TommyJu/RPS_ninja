795c17c00fd89c97f197a281658a7f37ea7e5aa0
Matthew Yoon <myoon11@my.bcit.ca>
2023-12-06 17:29:46 -0800

    add docstring to does_player_win function

--- combat/does_player_win.py
+++ combat/does_player_win.py
@@ -8,10 +8,16 @@ A01373290
 
 def does_player_win(player_action, enemy_action):
     """
+    Determines if player wins the combat encounter
 
-    :param player_action:
-    :param enemy_action:
-    :return:
+    :param player_action: tuple representing the user input and random attack value
+    :param enemy_action: tuple representing the randomly generated enemy action
+    :precondition: player_action is a tuple with index 0 being a string in the results list
+                   index 1 is an integer
+                   enemy_action is a tuple with index 0 being a string in the results list
+                   index 1 is an integer
+    :postcondition: correctly determines if player wins the encounter, True if player win, False if player loses
+    :return: Boolean representing player success in combat
     """
     choices = {'rock': 0, 'paper': 1, 'scissor': 2}
     result = choices[player_action[0]] - choices[enemy_action[0]]
