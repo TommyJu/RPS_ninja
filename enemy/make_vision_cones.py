990600d2ab5b1351c2cb7aa159d18377e0c48bf8
Tommy Ju <tommyju25@gmail.com>
2023-12-06 19:29:20 -0800

    Create tests for and refactor make_vision_cones

--- enemy/make_vision_cones.py
+++ enemy/make_vision_cones.py
@@ -11,40 +11,45 @@ def make_vision_cones(enemy_coordinates, board):
     """
     Create vision for all enemies in a random valid direction
 
+    A valid direction is not out of bounds, on an enemy, on the start point, or on the end point
+
     :param enemy_coordinates: a list of lists for each enemy coordinate
     :param board: a dictionary representing the game board
     :precondition: board must be a dictionary from the make_board function
     :postcondition: generate coordinates for enemy vision in a random and valid adjacent square
 
-    :return: a list of lists for each enemy vision cone, a None value represents an enemy looking out of bounds
+    :return: a list of lists for each enemy vision cone, a None value represents an enemy looking at an invalid square
     """
     vision_cones = []
     end_point = list(max(board.keys()))
     start_point = [0, 0]
+    # convert each coordinate tuple into a list
+    board_coordinates = [list(coordinate) for coordinate in board]
+    invalid_coordinates = enemy_coordinates[:]
+    invalid_coordinates.append(start_point)
+    invalid_coordinates.append(end_point)
+
     for enemy in enemy_coordinates:
         # Pick 1 of the 4 adjacent squares to an enemy at random
-        random_adjacent_x = random.choice([-1, 1])
-        random_adjacent_y = random.choice([-1, 1])
-
-        vision_cones.append([enemy[0] + random_adjacent_x, enemy[1] + random_adjacent_y])
+        direction = random.choice(["north", "east", "south", "west"])
+        match direction:
+            case "north":
+                vision_cones.append([enemy[0], enemy[1] - 1])
+            case "east":
+                vision_cones.append([enemy[0] + 1, enemy[1]])
+            case "south":
+                vision_cones.append([enemy[0], enemy[1] + 1])
+            case "west":
+                vision_cones.append([enemy[0] - 1, enemy[1]])
 
         # If the vision cone we have just added is on an invalid square, assign to None.
-        if vision_cones[:-1] not in board or vision_cones[:-1] == start_point or vision_cones[:-1] == end_point:
-            vision_cones[:-1] = None
+        if vision_cones[-1] not in board_coordinates or vision_cones[-1] in invalid_coordinates:
+            vision_cones[-1] = None
 
     return vision_cones
 
     # for enemy in enemy_coordinates:
-    #     direction = random.choice(["north", "east", "south", "west"])
-    #     match direction:
-    #         case"north":
-    #             vision_cones.append([enemy[0], enemy[1] - 1])
-    #         case "east":
-    #             vision_cones.append([enemy[0] + 1, enemy[1]])
-    #         case "south":
-    #             vision_cones.append([enemy[0], enemy[1] + 1])
-    #         case "west":
-    #             vision_cones.append([enemy[0] - 1, enemy[1]])
+
     # return vision_cones
 
 
--- enemy/test_make_vision_cones.py
+++ enemy/test_make_vision_cones.py
@@ -8,23 +8,60 @@ from unittest.mock import patch
 
 class Test(TestCase):
 
-    @patch('random.choice', side_effect=['east', 'north', 'north'])
-    def test_make_vision_cone_normal_course(self, _):
-        enemies1 = [[0, 1], [0, 2], [0, 3]]
-        expected = [[1, 1], [0, 1], [0, 2]]
-        actual = make_vision_cones(enemies1)
-        self.assertEquals(expected, actual)
+    @patch('random.choice', side_effect=["north", "south", "east", "west"])
+    def test_all_vision_cone_generation_directions(self, _):
+        board = {(0, 0): "A haunted mansion", (1, 0): "A haunted mansion", (2, 0): "A haunted mansion",
+                 (0, 1): "A haunted mansion", (1, 1): "A haunted mansion", (2, 1): "A haunted mansion",
+                 (0, 2): "A haunted mansion", (1, 2): "A haunted mansion", (2, 2): "A haunted mansion",
+                 (0, 3): "A haunted mansion", (1, 3): "A haunted mansion", (2, 3): "A haunted mansion"}
+        enemies = [[2, 3], [0, 0], [1, 0], [2, 1]]
+        #           north   south   east    west
+        expected = [[2, 2], [0, 1], [2, 0], [1, 1]]
+        actual = make_vision_cones(enemies, board)
+        self.assertEqual(expected, actual)
 
-    @patch('random.choice', side_effect=['west', 'north', 'north'])
-    def test_make_vision_cone_goes_outside_border(self, _):
-        enemies1 = [[0, 1], [1, 0], [0, 1]]
-        expected = [[-1, 1], [1, -1], [0, 0]]
-        actual = make_vision_cones(enemies1)
-        self.assertEquals(expected, actual)
+    @patch('random.choice', side_effect=["north", "south", "east", "west"])
+    def test_vision_cones_out_of_bounds(self, _):
+        board = {(0, 0): "A haunted mansion", (1, 0): "A haunted mansion", (2, 0): "A haunted mansion",
+                 (0, 1): "A haunted mansion", (1, 1): "A haunted mansion", (2, 1): "A haunted mansion",
+                 (0, 2): "A haunted mansion", (1, 2): "A haunted mansion", (2, 2): "A haunted mansion",
+                 (0, 3): "A haunted mansion", (1, 3): "A haunted mansion", (2, 3): "A haunted mansion"}
+        enemies = [[1, 0], [1, 3], [2, 1], [0, 1]]
+        expected = [None, None, None, None]
+        actual = make_vision_cones(enemies, board)
+        self.assertEqual(expected, actual)
 
-    @patch('random.choice', side_effect=['east', 'south'])
-    def test_make_vision_cone_can_double(self, _):
-        enemies1 = [[0, 1], [1, 0]]
-        expected = [[1, 1], [1, 1]]
-        actual = make_vision_cones(enemies1)
-        self.assertEquals(expected, actual)
+    @patch('random.choice', side_effect=["north", "south"])
+    def test_vision_cones_on_start_and_end(self, _):
+        board = {(0, 0): "A haunted mansion", (1, 0): "A haunted mansion", (2, 0): "A haunted mansion",
+                 (0, 1): "A haunted mansion", (1, 1): "A haunted mansion", (2, 1): "A haunted mansion",
+                 (0, 2): "A haunted mansion", (1, 2): "A haunted mansion", (2, 2): "A haunted mansion",
+                 (0, 3): "A haunted mansion", (1, 3): "A haunted mansion", (2, 3): "A haunted mansion"}
+        enemies = [[0, 1], [2, 2]]
+        expected = [None, None]
+        actual = make_vision_cones(enemies, board)
+        self.assertEqual(expected, actual)
+
+    @patch('random.choice', side_effect=["north", "south"])
+    def test_vision_cones_on_enemy(self, _):
+        board = {(0, 0): "A haunted mansion", (1, 0): "A haunted mansion", (2, 0): "A haunted mansion",
+                 (0, 1): "A haunted mansion", (1, 1): "A haunted mansion", (2, 1): "A haunted mansion",
+                 (0, 2): "A haunted mansion", (1, 2): "A haunted mansion", (2, 2): "A haunted mansion",
+                 (0, 3): "A haunted mansion", (1, 3): "A haunted mansion", (2, 3): "A haunted mansion"}
+        enemies = [[0, 2], [0, 1]]
+        expected = [None, None]
+        actual = make_vision_cones(enemies, board)
+        self.assertEqual(expected, actual)
+    # @patch('random.choice', side_effect=['west', 'north', 'north'])
+    # def test_make_vision_cone_goes_outside_border(self, _):
+    #     enemies1 = [[0, 1], [1, 0], [0, 1]]
+    #     expected = [[-1, 1], [1, -1], [0, 0]]
+    #     actual = make_vision_cones(enemies1)
+    #     self.assertEquals(expected, actual)
+    #
+    # @patch('random.choice', side_effect=['east', 'south'])
+    # def test_make_vision_cone_can_double(self, _):
+    #     enemies1 = [[0, 1], [1, 0]]
+    #     expected = [[1, 1], [1, 1]]
+    #     actual = make_vision_cones(enemies1)
+    #     self.assertEquals(expected, actual)
