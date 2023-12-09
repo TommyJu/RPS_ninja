bc5e1a4d57eeafe0fa7a08837f89f89485803e36
Tommy Ju <tommyju25@gmail.com>
2023-12-09 09:51:27 -0800

    Modify game instance and get_user_choice to use GUI

--- game.py
+++ game.py
@@ -52,7 +52,6 @@ def game():
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(1, weight=2)
 
-
     canvas = tk.Canvas(root)
     canvas.grid(row=0, column=0, sticky="nsew")
 
@@ -103,9 +102,6 @@ def game():
     input_widget = ttk.Entry(canvas_right, textvariable=input_widget_value)
     input_widget.grid(row=6, column=0, sticky="nsew")
 
-    enter_widget = ttk.Button(canvas_right, text="ENTER")
-    enter_widget.grid(row=7, column=0, sticky="nsew")
-
     # Images
     ninja_image_file = Image.open("./assets/ninja-heroic-stance.png")
     ninja_image = ImageTk.PhotoImage(ninja_image_file.resize((30, 30)))
@@ -118,13 +114,12 @@ def game():
     end_point_image_file = Image.open("./assets/exit-door.png")
     end_point_image = ImageTk.PhotoImage(end_point_image_file.resize((30, 30)))
 
-    #widgets
+    # game instancewidgets
     ninja_widget = tk.Label(canvas, image=ninja_image, background="white")
     enemy_widgets = []
     vision_cone_widgets = []
     end_point_widget = tk.Label(canvas, image=end_point_image, background="white")
     end_point_widget.grid(row=end_point[0], column=end_point[1])
-
 
     # Create widgets
     for index in range(len(enemies)):
@@ -140,7 +135,6 @@ def game():
     # Add background image to canvas
     canvas.create_image(0, 0, image=level_one_image)
 
-
     def update_widgets(user_interface_data):
         canvas.create_image(0, 0, image=level_one_image)
         ninja_widget.grid(row=user_interface_data["character"][1], column=user_interface_data["character"][0])
@@ -155,72 +149,72 @@ def game():
 
     update_widgets(data)
 
-
     def game_instance():
-        while True:
-            # Check if endpoint reached or character is dead
-            achieved_goal = check_if_endpoint_reached(character, board)
-            character_still_alive = is_alive(character)
-            if achieved_goal:
-                # enemies = make_enemy(board)
-                # vision_cones = make_vision_cones(board)
-                enemies.clear()
-                vision_cones.clear()
-                for enemy_widget, vision_cone_widget in zip(enemy_widgets, vision_cone_widgets):
-                    enemy_widget.destroy()
-                    vision_cone_widget.destroy()
-                enemy_widgets.clear()
-                vision_cone_widgets.clear()
-                print(enemies)
-                print(vision_cones)
-                # Create new widgets
-                enemies.extend(make_enemy(board))
-                vision_cones.extend(make_vision_cones(enemies, board))
+        # while True:
+        # Check if endpoint reached or character is dead
+        achieved_goal = check_if_endpoint_reached(character, board)
+        character_still_alive = is_alive(character)
+        if achieved_goal:
+            # enemies = make_enemy(board)
+            # vision_cones = make_vision_cones(board)
+            enemies.clear()
+            vision_cones.clear()
+            for enemy_widget, vision_cone_widget in zip(enemy_widgets, vision_cone_widgets):
+                enemy_widget.destroy()
+                vision_cone_widget.destroy()
+            enemy_widgets.clear()
+            vision_cone_widgets.clear()
+            print(enemies)
+            print(vision_cones)
+            # Create new widgets
+            enemies.extend(make_enemy(board))
+            vision_cones.extend(make_vision_cones(enemies, board))
 
-                for index in range(len(enemies)):
-                    enemy_widget = tk.Label(canvas, image=enemy_image, background="white")
-                    enemy_widget.lift()
-                    enemy_widgets.append(enemy_widget)
-                for index in range(len(vision_cones)):
-                    vision_cone_widget = tk.Label(canvas, image=vision_cone_image, background="white")
-                    vision_cone_widgets.append(vision_cone_widget)
-                print(enemy_widgets)
+            for index in range(len(enemies)):
+                enemy_widget = tk.Label(canvas, image=enemy_image, background="white")
+                enemy_widget.lift()
+                enemy_widgets.append(enemy_widget)
+            for index in range(len(vision_cones)):
+                vision_cone_widget = tk.Label(canvas, image=vision_cone_image, background="white")
+                vision_cone_widgets.append(vision_cone_widget)
+            print(enemy_widgets)
 
-                # Reset character position
-                character["X-coordinate"] = 0
-                character["Y-coordinate"] = 0
+            # Reset character position
+            character["X-coordinate"] = 0
+            character["Y-coordinate"] = 0
 
-                new_level_data = get_user_interface_data(character, end_point, enemies)
-                update_widgets(new_level_data)
-            elif not character_still_alive:
-                break
+            new_level_data = get_user_interface_data(character, end_point, enemies)
+            update_widgets(new_level_data)
+        elif not character_still_alive:
+            # break
+            return
 
-            # Move character
-            direction = get_user_choice(character, board)
-            move_character(character, direction)
+        # Move character
+        direction = get_user_choice(character, board, input_widget_value.get())
+        move_character(character, direction)
 
+        # Move enemies and update GUI
+        enemies_move(enemies, vision_cones, board)
+        describe_current_location(board, character)
+        instance_data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
+        update_widgets(instance_data)
 
-            # Move enemies and update GUI
-            enemies_move(enemies, vision_cones, board)
-            describe_current_location(board, character)
-            instance_data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
-            update_widgets(instance_data)
+        # Check for combat, then update GUI after combat
+        enemy_detected_by_index = enemy_detection(character, enemies, vision_cones)
+        if enemy_detected_by_index != None:
+            is_combat_won = engage_combat(character)
+            if is_combat_won:
+                delete_enemy(enemies, enemy_widgets, vision_cones, vision_cone_widgets, enemy_detected_by_index)
+                instance_data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
+                update_widgets(instance_data)
 
-            # Check for combat, then update GUI after combat
-            enemy_detected_by_index = enemy_detection(character, enemies, vision_cones)
-            if enemy_detected_by_index != None:
-                is_combat_won = engage_combat(character)
-                if is_combat_won:
-                    delete_enemy(enemies, enemy_widgets, vision_cones, vision_cone_widgets, enemy_detected_by_index)
-                    instance_data = get_user_interface_data(character, end_point, [(2, 2), (1, 1)])
-                    update_widgets(instance_data)
-
-
-        print("Game end")
-
+    print("Game end")
 
     # Create a separate thread for the game loop
-    threading.Thread(target=game_instance).start()
+    # threading.Thread(target=game_instance).start()
+    enter_widget = ttk.Button(canvas_right, text="ENTER", command=game_instance)
+    enter_widget.grid(row=7, column=0, sticky="nsew")
+
     root.mainloop()
 
 
--- movement/get_user_choice.py
+++ movement/get_user_choice.py
@@ -8,7 +8,7 @@ A01373290
 from movement.validate_move import validate_move
 
 
-def get_user_choice(character, board):
+def get_user_choice(character, board, user_input):
     """
     Prompt the user to pick a direction
 
@@ -28,24 +28,29 @@ def get_user_choice(character, board):
                          ["east", "e", "3"],
                          ["west", "w", "4"]]
 
-    while True:
-        user_input = input("Where do you wish to go?\n"
-                           "1. (N)orth\n"
-                           "2. (S)outh\n"
-                           "3. (E)ast\n"
-                           "4. (W)est\n")
-        user_input = user_input.strip()
-        if user_input.lower() in acceptable_inputs:
-            for direction in equivalent_inputs:
-                if user_input.lower() in direction:
-                    if validate_move(board, character, direction[0]):
-                        return direction[0]
-                    else:
-                        print("move is not valid \n"
-                              "please keep movement on board")
-        else:
-            print("\nPlease enter a valid direction.\n"
-                  "To move north: type 'north', 'n', or '1'")
+    # while True:
+    # user_input = input("Where do you wish to go?\n"
+    #                    "1. (N)orth\n"
+    #                    "2. (S)outh\n"
+    #                    "3. (E)ast\n"
+    #                    "4. (W)est\n")
+    print("Where do you wish to go?\n"
+          "1. (N)orth\n"
+          "2. (S)outh\n"
+          "3. (E)ast\n"
+          "4. (W)est\n")
+    user_input = user_input.strip()
+    if user_input.lower() in acceptable_inputs:
+        for direction in equivalent_inputs:
+            if user_input.lower() in direction:
+                if validate_move(board, character, direction[0]):
+                    return direction[0]
+                else:
+                    print("move is not valid \n"
+                          "please keep movement on board")
+    else:
+        print("\nPlease enter a valid direction.\n"
+              "To move north: type 'north', 'n', or '1'")
 
 
 def main():
