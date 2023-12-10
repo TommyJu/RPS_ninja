f02af5105199dd095c5b4d610721eab386d3d542
Matthew Yoon <myoon11@my.bcit.ca>
2023-12-09 16:02:55 -0800

    ADD docstirng to boss_combat

--- Boss/boss_combat.py
+++ Boss/boss_combat.py
@@ -16,8 +16,27 @@ from does_quick_time_occur import does_quick_time_occur
 from resolve_quick_time import resolve_quick_time
 
 
-def boss_combat(character: dict, boss: dict, attack_choice: str):
-    if is_boss_alive(boss) and is_alive(character):
+def boss_combat(character: dict, boss: dict, attack_choice: str) -> bool:
+    """
+    Run boss combat
+
+    determines if enemy widget needs to be removed
+    Alter boss and character function by player actions and randomly chosen boss action
+
+    :param character: dictionary representing player
+    :param boss: dictionary representing hp
+    :param attack_choice: string from the GUI input
+    :precondition: Correctly determines if widget must be removed, If false remove the widget,
+                   True if widget should be removed
+                   Character must be a dictionary with key "Current HP" and "Attack Level" with values being integers
+                   Boss must be a dictionary with key "Current HP"
+                   attack_choice must be a string
+    :postcondition: correctly determines if either boss of character dictionary is altered and by how much
+                    Correctly determine if combat results in false, widget should be remove
+                    or True, widget to be removed
+    :return: Boolean representing the result of combat, True if enemy is defeated, False otherwise
+    """
+    if is_alive(boss) and is_alive(character):
         print("You have:" + str(character["Current HP"]) + "HP")
         if does_quick_time_occur():
             quick_time_input = quick_time_event()
@@ -30,8 +49,10 @@ def boss_combat(character: dict, boss: dict, attack_choice: str):
         if does_player_win(player_action, enemy_action):
             if (player_action[1] - enemy_action[1]) > 0:
                 boss["Current HP"] -= (player_action[1] - enemy_action[1])
-                print("you won")
-                return False
+                if is_alive(boss):
+                    return True
+                else:
+                    return False
             else:
                 boss["Current HP"] -= 1
                 return False
@@ -46,7 +67,6 @@ def boss_combat(character: dict, boss: dict, attack_choice: str):
             else:
                 character["Current HP"] -= 1
     return False
-
 
 
 def main():
